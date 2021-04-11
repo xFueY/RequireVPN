d9bb9ca75b778d279f00b016d835469e8a1b2d54
xFueY <62284893+xFueY@users.noreply.github.com>
2021-04-11 20:17:04 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+Classifiers = [
+  "Development Status :: 5 - Production/Stable",
+  "Intended Audience :: Developers",
+  "License :: OSI Approved :: MIT License",
+  "Natural Language :: English",
+  "Programming Language :: Python :: 3.8"
+]
+
+setup(
+  name="RequireVPN",
+  version="0.0.1",
+  description="Python Library To Require VPN Connections",
+  long_description=open('README.md').read(),
+  long_description_content_type="text/markdown",
+  url="https://github.com/xFueY/RequireVPN/",
+  author="xFueY",
+  author_email="xFueY@protonmail.com",
+  license="MIT",
+  classifiers=Classifiers,
+  packages=find_packages(),
+  keywords="RequireVPN",
+  install_requires=["requests"]
+)
